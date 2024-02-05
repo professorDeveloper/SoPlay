8496d28eef6d4bea4541a5f2079a9cbc76c1d6be
professorDeveloper <azamovhud007@outlook.com>
2024-02-05 14:12:23 +0500

    Init project

---
+++ app/src/main/java/com/azamovhudstc/soplay/ui/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.azamovhudstc.soplay.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azamovhudstc.soplay.R
+import com.azamovhudstc.soplay.utils.initActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
